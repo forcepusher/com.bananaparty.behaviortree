871018c958430e236ef07b85ddf77c20f1bfaf1b
forcepusher <bionitsoup@gmail.com>
2022-01-30 15:57:06 +0700

    Remove duplicated code in rollback nodes

---
+++ Runtime/RollbackNodes/Snapshots/NodeSnapshot.cs
@@ -0,0 +1,22 @@
+﻿namespace BananaParty.BehaviorTree
+{
+    /// <summary>
+    /// Default <see cref="INodeSnapshot"/> implementation for nodes that don't have any state besides <see cref="BehaviorNodeStatus"/>.
+    /// </summary>
+    public class NodeSnapshot : INodeSnapshot
+    {
+        private readonly IBehaviorNode _behaviorNode;
+        private readonly BehaviorNodeStatus _status;
+
+        public NodeSnapshot(IBehaviorNode behaviorNode, BehaviorNodeStatus status)
+        {
+            _behaviorNode = behaviorNode;
+            _status = status;
+        }
+
+        public void ApplyState()
+        {
+            _behaviorNode.Status = _status;
+        }
+    }
+}
