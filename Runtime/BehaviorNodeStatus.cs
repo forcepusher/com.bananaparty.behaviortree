4b5f080d74c83fc1b62d5f8e2c76c0f0a12311d4
forcepusher <bionitsoup@gmail.com>
2022-02-01 20:30:15 +0700

    Add stopStatus to RepeatNode

--- Runtime/BehaviorNodeStatus.cs
+++ Runtime/BehaviorNodeStatus.cs
@@ -3,8 +3,8 @@
     public enum BehaviorNodeStatus
     {
         Idle,
+        Running,
         Failure,
-        Success,
-        Running
+        Success
     }
 }
--- Runtime/Nodes/Decorators/RepeatNode.cs
+++ Runtime/Nodes/Decorators/RepeatNode.cs
@@ -2,16 +2,28 @@
 {
     public class RepeatNode : DecoratorNode
     {
-        public RepeatNode(IBehaviorNode childNode) : base(childNode) { }
+        private readonly BehaviorNodeStatus? _stopStatus;
+
+        /// <summary>
+        /// If <paramref name="stopStatus"/> is specified, the node is guaranteed to end with that exact <see cref="BehaviorNodeStatus"/>.
+        /// </summary>
+        public RepeatNode(IBehaviorNode childNode, BehaviorNodeStatus? stopStatus = null) : base(childNode)
+        {
+            _stopStatus = stopStatus;
+        }
 
         public override BehaviorNodeStatus OnExecute(long time)
         {
-            if (ChildNode.Status == BehaviorNodeStatus.Success || ChildNode.Status == BehaviorNodeStatus.Failure)
+            if (ChildNode.Status > BehaviorNodeStatus.Running)
                 ChildNode.Reset();
 
-            ChildNode.Execute(time);
+            BehaviorNodeStatus childStatus = ChildNode.Execute(time);
+            if (_stopStatus.HasValue && childStatus == _stopStatus.Value)
+                return _stopStatus.Value;
 
             return BehaviorNodeStatus.Running;
         }
+
+        public override string Name => $"{base.Name}{(_stopStatus.HasValue ? $"Until{_stopStatus}" : string.Empty)}";
     }
 }
