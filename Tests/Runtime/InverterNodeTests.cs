47242e3236dc3cd57e3e6072ac15c84621f8f525
YooPita <YooPitaStudio@gmail.com>
2022-12-28 22:56:16 +0300

    Add InverterNode Tests

---
+++ Tests/Runtime/InverterNodeTests.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace BananaParty.BehaviorTree.Tests
+{
+    public class InverterNodeTests
+    {
+        [Test]
+        public void MustBeReturnFailure()
+        {
+            IBehaviorNode testNode = new InverterNode(
+                new InvocationTestNode(BehaviorNodeStatus.Success));
+
+            var resultStatus = testNode.Execute();
+
+            Assert.IsTrue(resultStatus == BehaviorNodeStatus.Failure);
+        }
+
+        [Test]
+        public void MustBeReturnSuccess()
+        {
+            IBehaviorNode testNode = new InverterNode(
+                new InvocationTestNode(BehaviorNodeStatus.Failure));
+
+            var resultStatus = testNode.Execute();
+
+            Assert.IsTrue(resultStatus == BehaviorNodeStatus.Success);
+        }
+    }
+}
