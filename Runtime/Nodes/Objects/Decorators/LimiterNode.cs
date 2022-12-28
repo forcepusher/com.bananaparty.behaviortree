cd4aeb3a5b817d68315d2c4d4841e15527b16d18
YooPita <YooPitaStudio@gmail.com>
2022-12-28 23:15:06 +0300

    LimiterNode Fix

--- Runtime/Nodes/Objects/Decorators/LimiterNode.cs
+++ Runtime/Nodes/Objects/Decorators/LimiterNode.cs
@@ -9,10 +9,27 @@
 
         /// <summary>
         /// Allows the child node to be executed the specified <paramref name="repetitionCount"/> of times, returning a value of Running.
+        /// If the node has executed the specified number of times and the node is still Running, it returns Failure.
         /// </summary>
         public LimiterNode(IBehaviorNode childNode, int repetitionCount = 1) : base(childNode)
         {
             _repetitionCount = repetitionCount > 0 ? repetitionCount : 1;
+        }
+
+        protected override BehaviorNodeStatus OnSuccess()
+        {
+            if (_currentCount >= _repetitionCount)
+                return base.OnSuccess();
+            base.OnRestart();
+            return BehaviorNodeStatus.Running;
+        }
+
+        protected override BehaviorNodeStatus OnFailure()
+        {
+            if (_currentCount >= _repetitionCount)
+                return base.OnFailure();
+            base.OnRestart();
+            return BehaviorNodeStatus.Running;
         }
 
         protected override BehaviorNodeStatus OnRunning()
@@ -24,7 +41,7 @@
         protected override BehaviorNodeStatus OnExecute()
         {
             _currentCount++;
-            return _currentCount >= _repetitionCount ? base.OnExecute() : BehaviorNodeStatus.Running;
+            return base.OnExecute();
         }
 
         protected override void OnRestart()
---
+++ Tests/Runtime/LimiterNodeTests.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+
+namespace BananaParty.BehaviorTree.Tests
+{
+    public class LimiterNodeTests
+    {
+        [Test]
+        public void MustBeExecutedSpecifiedNumberOfTimes()
+        {
+            var invocationTestNode = new InvocationTestNode(BehaviorNodeStatus.Success);
+            IBehaviorNode testNode = new LimiterNode(invocationTestNode, 2);
+
+            var resultStatus = testNode.Execute();
+            Assert.IsTrue(resultStatus == BehaviorNodeStatus.Running);
+            resultStatus = testNode.Execute();
+
+            Assert.IsTrue(invocationTestNode.ExecutionCount == 2);
+            Assert.IsTrue(resultStatus == BehaviorNodeStatus.Success);
+        }
+
+        [Test]
+        public void MustReturnFailureIfNodeRunning()
+        {
+            IBehaviorNode testNode = new LimiterNode(
+                new InvocationTestNode(BehaviorNodeStatus.Running));
+
+            var resultStatus = testNode.Execute();
+
+            Assert.IsTrue(resultStatus == BehaviorNodeStatus.Failure);
+        }
+    }
+}
